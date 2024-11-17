f52c28c9bca00e74c5078bed6dc882b226ed20cd
Arne <arnesacnussem@gmail.com>
2024-11-18 00:05:02 +0300

    remove unused variable

--- src/Controller.cs
+++ src/Controller.cs
@@ -1,19 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.Meilisearch;
 
 [Route("meilisearch")]
 [ApiController]
-public class Controller(ILogger<Controller> logger, MeilisearchClientHolder clientHolder) : ControllerBase
+public class Controller(MeilisearchClientHolder clientHolder) : ControllerBase
 {
     [HttpGet("status")]
     public Task<IActionResult> GetStatus()
     {
-        if (!clientHolder.Ok)
-        {
-            Plugin.Instance?.TryCreateMeilisearchClient().Wait();
-        }
+        if (!clientHolder.Ok) Plugin.Instance?.TryCreateMeilisearchClient().Wait();
 
         return Task.FromResult<IActionResult>(new JsonResult(new
         {
@@ -22,7 +18,7 @@ public class Controller(ILogger<Controller> logger, MeilisearchClientHolder clie
             meilisearchOk = clientHolder.Ok,
             lastIndex = Plugin.Instance?.Indexer.LastIndex?.ToString() ?? "Not yet indexed",
             lastIndexed = Plugin.Instance?.Indexer.LastIndexCount,
-            averageSearchTime = $"{Plugin.Instance?.AverageSearchTime}ms",
+            averageSearchTime = $"{Plugin.Instance?.AverageSearchTime}ms"
         }));
     }
 }
