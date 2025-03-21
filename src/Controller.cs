52a584511f82010021a85b89a421aa1ea397e7dd
Cody Robibero <cody@robibe.ro>
2025-03-20 18:07:21 -0600

    Cleanup config controller

--- src/Controller.cs
+++ src/Controller.cs
@@ -1,29 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediaBrowser.Common.Api;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Jellyfin.Plugin.Meilisearch;
 
 [Route("meilisearch")]
 [ApiController]
+[Authorize(Policy = Policies.RequiresElevation)]
 public class Controller(MeilisearchClientHolder clientHolder) : ControllerBase
 {
     [HttpGet("status")]
-    public Task<IActionResult> GetStatus()
+    public ActionResult GetStatus()
     {
-        return Task.FromResult<IActionResult>(new JsonResult(new
+        return new JsonResult(new
         {
             db = Plugin.Instance?.DbPath,
             meilisearch = clientHolder.Status,
             meilisearchOk = clientHolder.Ok,
-            lastIndex = Plugin.Instance?.Indexer.LastIndex?.ToString() ?? "Not yet indexed",
-            lastIndexed = Plugin.Instance?.Indexer.LastIndexCount,
-            averageSearchTime = $"{Plugin.Instance?.AverageSearchTime}ms"
-        }));
+            lastIndex = Plugin.Instance!.Indexer.LastIndex?.ToString() ?? "Not yet indexed",
+            lastIndexed = Plugin.Instance.Indexer.LastIndexCount,
+            averageSearchTime = $"{Plugin.Instance.AverageSearchTime}ms"
+        });
     }
     
     [HttpGet("reconnect")]
-    public Task<IActionResult> Reconnect()
+    public async Task<ActionResult> Reconnect()
     {
-        if (!clientHolder.Ok) Plugin.Instance?.TryCreateMeilisearchClient().Wait();
+        if (!clientHolder.Ok) await Plugin.Instance!.TryCreateMeilisearchClient();
         return GetStatus();
     }
 }
